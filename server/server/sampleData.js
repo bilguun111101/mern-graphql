c04f73d3a7420836b326957652721953fd64c089
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2023-04-22 08:46:50 +0800

    Making

---
+++ server/server/sampleData.js
@@ -0,0 +1,98 @@
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Tony Stark',
+      email: 'ironman@gmail.com',
+      phone: '343-567-4333',
+      information: {
+        isHero: 'yes',
+        isMan: 'yes'
+      }
+    },
+    {
+      id: '2',
+      name: 'Natasha Romanova',
+      email: 'blackwidow@gmail.com',
+      phone: '223-567-3322',
+      information: {
+        isHero: true,
+        isMan: true
+      }
+    },
+    {
+      id: '3',
+      name: 'Thor Odinson',
+      email: 'thor@gmail.com',
+      phone: '324-331-4333',
+      information: {
+        isHero: true,
+        isMan: false
+      }
+    },
+    {
+      id: '4',
+      name: 'Steve Rogers',
+      email: 'steve@gmail.com',
+      phone: '344-562-6787',
+      information: {
+        isHero: true,
+        isMan: true
+      }
+    },
+    {
+      id: '5',
+      name: 'Bruce Banner',
+      email: 'bruce@gmail.com',
+      phone: '321-468-8887',
+      information: {
+        isHero: true,
+        isMan: true
+      }
+    },
+  ];
+  
+  module.exports = { projects, clients };
